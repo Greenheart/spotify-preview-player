cb3d8f26612f1d319c9db0f64bc00c7eec201482
Samuel Plumppu <greenheartmusic@gmail.com>
2020-09-27 22:56:57 +0200

    MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [songId, setSongId] = useState('0dINQNECUzmrGu6hYRiNPx')
+
+    const updateSong = (event: ChangeEvent<HTMLInputElement>) => {
+        const spotifyURI = event.target.value
+        const res = spotifyURI.split('spotify:track:')
+
+        if (res && res.length === 2) {
+            setSongId(res[1])
+        } else {
+            console.log('invalid song ID', spotifyURI, res)
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="uri-input">
+                <label htmlFor="spotify-uri">
+                    Enter a Spotify URI to preview:
+                </label>
+                <input
+                    type="text"
+                    name="spotify-uri"
+                    id="spotify-uri"
+                    autoFocus
+                    onChange={updateSong}
+                />
+            </div>
+            {songId ? (
+                <iframe
+                    title={'Spotify Song Preview for track: ' + songId}
+                    src={'https://open.spotify.com/embed/track/' + songId}
+                    width="300"
+                    height="380"
+                    frameBorder="0"
+                    allowTransparency
+                    allow="encrypted-media"></iframe>
+            ) : null}
+        </div>
+    )
 }
 
-export default App;
+export default App
