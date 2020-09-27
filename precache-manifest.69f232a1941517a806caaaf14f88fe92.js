b21110d7aa3b6179d28dbe4faff3d0c81c9a7f5b
Samuel Plumppu <greenheartmusic@gmail.com>
2020-09-27 23:04:43 +0200

    Updates

---
+++ precache-manifest.69f232a1941517a806caaaf14f88fe92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4cbd580e78012bf37aac55f6644764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e90ed79e1fbc280b4d51",
+    "url": "/static/css/main.416ba9f3.chunk.css"
+  },
+  {
+    "revision": "4ea5934cc1eddf854897",
+    "url": "/static/js/2.713d8aef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.713d8aef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e90ed79e1fbc280b4d51",
+    "url": "/static/js/main.d2f01364.chunk.js"
+  },
+  {
+    "revision": "249d9d66ec793578b108",
+    "url": "/static/js/runtime-main.637c3a39.js"
+  }
+]);
