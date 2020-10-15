e0da57e8b4bfca9bb20ddb75a74f00bcb4d67153
Samuel Plumppu <greenheartmusic@gmail.com>
2020-10-15 22:58:40 +0200

    update link in UI

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App = () => {
 
         if (match && match.length === 2) {
             setSongId(match[1])
+            event.target.value = 'https://open.spotify.com/track/' + match[1]
         } else if (event.target.value === '') {
             setSongId(DEFAULT_SONG_ID)
         } else {
